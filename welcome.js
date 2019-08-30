609bb819517690098b3167cf0d074a9ff9ac8932
xxbivo <45272544+xxbivo@users.noreply.github.com>
2019-08-30 17:42:53 +0200

    Add files via upload

---
+++ welcome.js
@@ -0,0 +1,16 @@
+﻿var Discord = require('discord.js');
+var fs = require('fs');
+var client = new Discord.Client();
+client.on("guildMemberAdd", member => {
+        if(member.guild.id === "441615240006729728") { 
+  const channel = member.guild.channels.find('id', '617008886796517377'); 
+if (!channel) return;
+channel.send(`**<@${member.user.id}> Welcome To __Gate Server__  ** ❤️ `)  
+}});
+client.on("guildMemberRemove", member => {
+        if(member.guild.id === "441615240006729728") { 
+  const channel = member.guild.channels.find('id', '617008886796517377'); 
+if (!channel) return;
+  channel.send(`**${member.user.tag} Left The Server  ** 😭 `) 
+}});
+client.login('Token');
